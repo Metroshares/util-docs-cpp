0c2bd9ce8e51fbc447f6cc4887dfd42457c14f67
dskvr <hi.na3s@gmail.com>
2018-10-26 15:09:12 +0800

    changes

--- scripts/build.sh
+++ scripts/build.sh
@@ -31,10 +31,10 @@ PY_VER=$(python -c 'import sys; print(".".join(map(str, sys.version_info[:3])))'
 PY_VER_SEM=( ${PY_VER//./ } )
 PY_VER_MAJOR=${PY_VER%.*.*}
 
-if(PY_VER_MAJOR != "3") {
+if("$PY_VER_MAJOR" != "3"); then
   python3 -m venv ~/.virtualenvs/myvenv
   source ~/.virtualenvs/myvenv/bin/activate
-}
+fi
 
 doxybook -i $PATH_BUILD/xml -o $PATH_BUILD/gitbook -s $PATH_BUILD/gitbook/SUMMARY.md -t gitbook
 deactivate
