283e17c98f32972fa739e29dc2d534f6591fa3e2
dskvr <hi.na3s@gmail.com>
2018-10-26 13:41:15 +0800

    changes

--- scripts/serve.sh
+++ scripts/serve.sh
@@ -3,4 +3,4 @@ PATH_CONFIG=$ABS_PATH/.docs
 # Positional
 PATH_SERVE=${1-"$ABS_PATH/tmp"} #path to build directory
 
-$PATH_CONFIG/node_modules/.bin/gitbook serve $PATH_SERVE
+$PATH_CONFIG/node_modules/.bin/gitbook serve $PATH_SERVE/gitbook
