cb67b96c4d68aed6b118719c0bf745bbccae5a54
dskvr <hi.na3s@gmail.com>
2018-10-31 13:41:26 +0800

    changes

--- versions.js
+++ versions.js
@@ -4,10 +4,11 @@ const program = require('commander');
 
 program
   .version('0.1.0')
-  .option('-p, --path', 'Absolute path to directory containing "versions" directories [docs/history]', 'history')
+  .option('-p, --path', 'Absolute path to directory containing "versions" directories [docs/history]', 'docs/history')
   .parse(process.argv);
 
 fs.readdir(program.path, (err, files) => {
+  console.log(files)
   files.forEach(file => {
     console.log(file);
   });
