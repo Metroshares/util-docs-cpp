a618d49284340d3be4942869c2a00d21d041a99f
dskvr <hi.na3s@gmail.com>
2018-11-01 15:02:30 +0800

    config overrides

--- config.js
+++ config.js
@@ -11,8 +11,8 @@ let obj = {}
 const user_config = JSON.parse( fs.readFileSync("eosio.docs.json") )
 const config = JSON.parse( fs.readFileSync(".docs/book.default.json") )
 
-config.logo = config["pluginsConfig"]["custom-header-next"].logo.replace("${project}", user_config.project)
-config.base_path = config["pluginsConfig"]["custom-header-next"].base_path.replace("${project}", user_config.project)
-config.text = config["pluginsConfig"]["custom-header-next"].text.replace("${project}", user_config.project)
+config["pluginsConfig"]["custom-header-next"].logo = config["pluginsConfig"]["custom-header-next"].logo.replace("${project}", user_config.project)
+config["pluginsConfig"]["custom-header-next"].base_path = config["pluginsConfig"]["custom-header-next"].base_path.replace("${project}", user_config.project)
+config["pluginsConfig"]["custom-header-next"].text = config["pluginsConfig"]["custom-header-next"].text.replace("${project}", user_config.project)
 
 fs.writeFile('.docs/book.json', JSON.stringify(config), 'utf8', () => {});
